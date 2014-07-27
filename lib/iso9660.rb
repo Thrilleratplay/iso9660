8b03ace30e7c697fdce8c13dd3ee9d1f691d1cde
Tom Hiller <thilleratplay@gmail.com>
2014-07-27 19:05:46 -0400

    Clean up cruft

--- lib/iso9660.rb
+++ lib/iso9660.rb
@@ -26,7 +26,6 @@ class Iso
   #
   # * *Args*    :
   #   - +stream+ -> Open file stream
-  #   - +offset+ -> Byte offset
   # * *Raises* :
   #   - +Error+ -> if system endianness cannot be determined
   def initialize(stream = nil)
@@ -70,31 +69,13 @@ class Iso
         end
       end
     end
-=begin
-  @logical_block_size = @volume_descriptor.primary.data.logical_block_size
-
-  path_table_loc = if @endianness == :little
-  @volume_descriptor.primary.data.path_table_l_loc
-  else
-  @volume_descriptor.primary.data.path_table_m_loc
-  end
-
-  @path_table = PathTable.new(@stream,path_table_loc ,@logical_block_size, @volume_descriptor.primary.data.path_table_size)
-  @directories = Directories.new
-
-  @path_table.paths.each{ |f|
-  stream.pos = f[:lba] * @logical_block_size
-  directories.parse_sector(stream.read(@logical_block_size))
-  }
-=end
   end
 
   # stream dump if stream set
   #
   # * *Args*    :
   #   - +stream+ -> Open file stream
-  #   - +offset+ -> Byte offset
-  def dump(stream = nil, offset = Offset.THIRTY_TWO_BYTES)
+  def dump(stream = nil)
     # TODO fill me in
   end
 
